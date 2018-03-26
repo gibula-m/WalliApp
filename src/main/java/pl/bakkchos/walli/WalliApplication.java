88d2413a164702b11bfd2420060c059ac3dab76b
Mateusz Gibula <gibulam94@gmail.com>
2018-03-26 09:02:02 +0200

    initial commit

---
+++ src/main/java/pl/bakkchos/walli/WalliApplication.java
@@ -0,0 +1,12 @@
+package pl.bakkchos.walli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalliApplication.class, args);
+	}
+}
